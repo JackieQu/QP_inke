6888ebaf47ff3c14b0eae63ad97a1feaa636135f
JackieQu <13263699826@163.com>
2016-11-08 01:23:33 +0800

    添加 pch, icon

---
+++ QP_inke/Classes/Other/QPMacros.h
@@ -0,0 +1,17 @@
+//
+//  QPMacros.h
+//  QP_inke
+//
+//  Created by 曲鹏 on 2016/11/8.
+//  Copyright © 2016年 JackieQu. All rights reserved.
+//
+
+#ifndef QPMacros_h
+#define QPMacros_h
+
+#define SCREEN_WIDTH  [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+
+#define RGB(r,g,b) [UIColor colorWithRed:(r/255.0) green:(g/255.0) blue:(b/255.0) alpha:1]
+
+#endif /* QPMacros_h */
