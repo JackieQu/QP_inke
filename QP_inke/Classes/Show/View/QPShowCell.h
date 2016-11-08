0a105ce90b8bb2f784aecd0b2687f62634a53dd1
JackieQu <13263699826@163.com>
2016-11-08 18:56:55 +0800

    约束 hotcell

---
+++ QP_inke/Classes/Show/View/QPShowCell.h
@@ -0,0 +1,13 @@
+//
+//  QPShowCell.h
+//  QP_inke
+//
+//  Created by 曲鹏 on 2016/11/8.
+//  Copyright © 2016年 JackieQu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QPShowCell : UITableViewCell
+
+@end
---
+++ QP_inke/Classes/Show/View/QPShowCell.m
@@ -0,0 +1,24 @@
+//
+//  QPShowCell.m
+//  QP_inke
+//
+//  Created by 曲鹏 on 2016/11/8.
+//  Copyright © 2016年 JackieQu. All rights reserved.
+//
+
+#import "QPShowCell.h"
+
+@implementation QPShowCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
