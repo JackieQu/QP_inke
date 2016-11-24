5cb85d4f4054542c5cee14b9750a4ed02010bc23
JackieQu <13263699826@163.com>
2016-11-24 14:01:42 +0800

    添加倒计时

--- QP_inke/Classes/Show/View/QPAdView.m
+++ QP_inke/Classes/Show/View/QPAdView.m
@@ -11,11 +11,15 @@
 #import "QPAd.h"
 #import "QPCacheHelper.h"
 
+static NSInteger showTime = 3;
+
 @interface QPAdView ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *backView;
 
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+
+@property (nonatomic, strong) dispatch_source_t timer;
 
 @end
 
@@ -87,7 +91,47 @@
 
 - (void)startTimer {
     
+    __block NSUInteger timeout = showTime + 1;
     
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_global_queue(0, 0));
+    
+    self.timer = timer;
+    
+    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(timer, ^{
+        
+        if (timeout <= 0) {
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self dismiss];
+            });
+            
+        } else {
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.timeLabel.text = [NSString stringWithFormat:@"跳过: %zd",timeout];
+            });
+            
+            timeout --;
+            
+        }
+        
+    });
+    dispatch_resume(timer);
+    
+}
+
+- (void)dismiss {
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        
+        self.alpha = 0.f;
+        
+    } completion:^(BOOL finished) {
+        
+        [self removeFromSuperview];
+        
+    }];
     
 }
 
